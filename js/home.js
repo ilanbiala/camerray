9445ef8d2d3d48c4e7c63c74af12603af7f27565
jin03002 <kimildoo12@gmail.com>
2017-01-18 20:33:02 -0500

    Added basic home page

---
+++ js/home.js
@@ -0,0 +1,13 @@
+function takePhoto() {
+  document.getElementById("photo_taken").innerHTML = "✗";
+  //do_stuff();
+
+  document.getElementById("photo_taken").innerHTML = "✓";
+}
+
+function takeVideo() {
+  document.getElementById("video_taken").innerHTML = "✗";
+  //do_stuff();
+
+  document.getElementById("video_taken").innerHTML = "✓";
+}
