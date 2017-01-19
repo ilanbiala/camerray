c3f3a5eed1edb367015982c364b3825f409c79d2
jin03002 <kimildoo12@gmail.com>
2017-01-19 13:25:01 -0500

    Use Electron

--- js/home.js
+++ js/home.js
@@ -1,10 +1,45 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+var win;
+
+function createWindow () {
+  win = new BrowserWindow({width: 800, height: 600});
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, '../home.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win = null
+  });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
+
 function takePhoto() {
   document.getElementById("photo_taken").innerHTML = "✗";
 
-  $.ajax({
-    type: "POST",
-    url: "~/take_pic.py"
-  }).done(function( o ) {
+  PythonShell.run('take_pics.py', function (err) {
+    if (err) throw err;
+    console.log('pics taken');
     document.getElementById("photo_taken").innerHTML = "✓";
   });
 }
