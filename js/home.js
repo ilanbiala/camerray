f1aa1ee5620b9cefc18197e6705dce82ce25c854
jin03002 <kimildoo12@gmail.com>
2017-01-18 20:49:44 -0500

    Call python functions from button

--- js/home.js
+++ js/home.js
@@ -1,13 +1,21 @@
 function takePhoto() {
   document.getElementById("photo_taken").innerHTML = "✗";
-  //do_stuff();
 
-  document.getElementById("photo_taken").innerHTML = "✓";
+  $.ajax({
+    type: "POST",
+    url: "~/take_pic.py"
+  }).done(function( o ) {
+    document.getElementById("photo_taken").innerHTML = "✓";
+  });
 }
 
 function takeVideo() {
   document.getElementById("video_taken").innerHTML = "✗";
-  //do_stuff();
 
-  document.getElementById("video_taken").innerHTML = "✓";
+  $.ajax({
+    type: "POST",
+    url: "~/take_vid.py"
+  }).done(function( o ) {
+    document.getElementById("video_taken").innerHTML = "✓";
+  });
 }
